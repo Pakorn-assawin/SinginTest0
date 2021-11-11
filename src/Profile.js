f031ec26e1cf7c0666e3e55a4c246282ea0baebd
Pakorn-assawin <70192843+Pakorn-assawin@users.noreply.github.com>
2021-11-11 15:49:11 +0700

    ok

--- src/Profile.js
+++ src/Profile.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom'  
+import { makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
@@ -7,7 +9,8 @@ import MenuItem from '@material-ui/core/MenuItem';
 import Menu from '@material-ui/core/Menu';
 import Avatar from '@material-ui/core/Avatar';
 import Button from '@material-ui/core/Button';
-
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
 
 const useStyles = makeStyles((theme) => ({
   root: {

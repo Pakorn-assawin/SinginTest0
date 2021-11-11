15d5cf1b00d9f6da46726b068f4b171f74d73063
Pakorn-assawin <70192843+Pakorn-assawin@users.noreply.github.com>
2021-11-11 13:30:58 +0700

    deploy now

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Signin from './Signin';
 import Profile from './Profile';
-
+import Aiforpic from './Components/Aiforpic.js';
 
 
 
@@ -32,7 +32,11 @@ function App() {
 
           </Route>
 
-         
+          <Route path="/src/Aiforpic.js">
+            <Aiforpic />
+
+          </Route>
+          
         </Switch>
       </BrowserRouter>
     </div>
